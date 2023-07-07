e7728ffa1542a60a7cd91447071a9070ec9d7a08
Sokolova-LV <leyla-@ukr.net>
2023-07-07 10:42:18 +0300

    changed 7.07

--- src/components/Notification/Notification.styled.jsx
+++ src/components/Notification/Notification.styled.jsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
 
 export const Text = styled.p`
+    text-align: center;
+    color: green;
 `;
--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,13 +1,10 @@
 import styled from '@emotion/styled';
 
 export const List = styled.ul`
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 10px
+    list-style: none;
 `;
 
 export const Item = styled.li`
-    font-size: 18px;
-    font-weigth: 500;
+    font-size: 20px;
+    color: orchid;
 `;
